bb6496f580664ac5a41d670609b1197ea504736c
Radical <benjaminjohnsen.bgj@gmail.com>
2022-05-25 18:13:12 +0200

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+// Create functions for fizz, buzz and fizzbuzz that return bools for whether the number is divisible by 3, 5 or both respectively.
 fn fizz(i: i32) -> bool {
     if i % 3 == 0 {
+        // Return true if i is divisible by 3
         return true;
     } else {
         return false;
@@ -8,6 +10,7 @@ fn fizz(i: i32) -> bool {
 
 fn buzz(i: i32) -> bool {
     if i % 5 == 0 {
+        // Return true if i is divisible by 5
         return true;
     } else {
         return false;
@@ -16,20 +19,27 @@ fn buzz(i: i32) -> bool {
 
 fn fizzbuzz(i: i32) -> bool {
     if i % 5 == 0 && i % 3 == 0 {
+        // Return true if i is divisible by 3 and 5
         return true;
     } else {
         return false;
     }
 }
 
+// Main function for execution
 fn main() {
-    for i in 1..101 {
+    // For loop to iterate through numbers from 1 to 100
+    for i in 1..=100 {
+        // If i is divisible by 3 and 5, print fizzbuzz
         if fizzbuzz(i) {
             println!("fizzbuzz!")
+        // If i is divisible by 3, print fizz
         } else if fizz(i) {
             println!("fizz")
+        // If i is divisible by 5, print buzz
         } else if buzz(i) {
             println!("buzz")
+        // If i is not divisible by 3 or 5, print i
         } else {
             println!("{}", i)
         }
